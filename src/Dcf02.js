93aa852bd063c374806b4b2938436c4c8cbf1ba9
UtsavHere <utsavpatel9719@gmail.com>
2023-12-28 23:48:17 +0530

    v1

---
+++ src/Dcf02.js
@@ -0,0 +1,12 @@
+import React from "react";
+import logo from "./dcf2.JPG"
+
+function Dcf02(){
+    return(
+        <div>
+            <h1 style={{ backgroundColor: "black" , color:"GrayText"}}>DigiCrafts frames</h1>
+      <img width={500} src={logo} />
+        </div>
+    )
+}
+export default Dcf02;
